c55bf5581150cb596be178b995069a337a050614
Rahul Nishant <rahulnishant13@gmail.com>
2021-07-21 20:03:19 +0530

    added topK and minimu sub size

--- MinimumSizeSubarraySum.java
+++ MinimumSizeSubarraySum.java
@@ -1,2 +1,34 @@
-package LeetCode_Questions;public class MinimumSizeSubarraySum {
+package LeetCode_Questions;
+
+public class MinimumSizeSubarraySum {
+    public static void main(String[] args) {
+        int target = 11;
+        int [] nums = new int[]{1,2,3,4,5};
+        System.out.println(minSubArrayLen(target, nums));
+    }
+
+    private static int minSubArrayLen(int target, int[] nums) {
+        int sum = 0, count = 0;
+        boolean baba = true;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0, k = 0; i < nums.length || k < nums.length;) {
+            if (target > sum && i < nums.length){
+                sum += nums[i];
+                i++;
+                count++;
+            }
+            else if (target == sum && baba){
+                baba = false;
+                min = Math.min(min, count);
+            }
+            else if (k < nums.length) {
+                baba = true;
+                sum -= nums[k];
+                k++;
+                count--;
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
 }
--- TopKFrequentElements.java
+++ TopKFrequentElements.java
@@ -1,2 +1,63 @@
-package LeetCode_Questions;public class TopKFrequentElements {
+package LeetCode_Questions;
+
+import java.util.*;
+/*
+1 1 1 2 2 1 3 3 3 4 3 4
+ 1:4
+ 2:2
+ 3:4
+ 4:2
+ k=3
+ 3 1 4 2
+
+ */
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+        int [] nums = new int[]{1,1,1,2,2,1,3,3,3,4,3,4};
+        int k = 3;
+        nums = topKFrequent(nums, k);
+        for ( int num : nums) System.out.println(" " + num);
+    }
+    private static int[] topKFrequent(int[] nums, int k) {
+        int [] result = new int [k];
+        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+
+        for (Integer num : nums){
+            if (countMap.containsKey(num)) {
+                countMap.put(num, countMap.get(num) + 1);
+            } else {
+                countMap.put(num, 1);
+            }
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(countMap.entrySet());
+//        Collections.sort(list, (a, b) -> a.getValue() > b.getValue() ? -1 :
+//                a.getValue() == b.getValue() ? (a.getKey() > b.getKey() ? -1 : 1) : 1);
+
+//        Collections.sort(list, (a,b) -> b.getValue().compareTo(a.getValue()));
+
+//        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+//            @Override
+//            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+//                if(o1.getValue() > o2.getValue())
+//                    return -1;
+//                if(o1.getValue() == o2.getValue())
+//                    return o1.getKey() > o2.getKey() ? -1 : 1;
+//
+//                return 1;
+//            }
+//        });
+
+        Collections.sort(list, (a,b) -> b.getValue().equals(a.getValue())
+                                ? b.getKey().compareTo(a.getKey())
+                                : b.getValue().compareTo(a.getValue())
+                        );
+
+
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i).getKey();
+        }
+
+        return result;
+    }
 }
