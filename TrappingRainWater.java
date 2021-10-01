b5b93aa17fd9dd30a928e1830dd539fcb865ad9b
Rahul Nishant <rahulnishant13@gmail.com>
2021-10-01 14:38:00 +0530

    TrappingRainWater

---
+++ TrappingRainWater.java
@@ -0,0 +1,35 @@
+package LeetCode_Questions;
+
+import java.util.Arrays;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        int [] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println("trap water: "+ trap(height));
+    }
+
+    private static int trap(int[] height) {
+        int result =0;
+        int [] lr_min_height = new int[height.length];
+        int min_h = 0;
+        for (int i = 0; i < height.length; i++) {
+            lr_min_height[i] = min_h = Math.max(min_h, height[i]);
+        }
+
+        System.out.println(Arrays.toString(lr_min_height));
+
+        min_h = 0;
+
+        for (int i = height.length-1; i >=0; i--) {
+            min_h = Math.max(height[i], min_h);
+            lr_min_height[i] = min_h = Math.min(min_h, lr_min_height[i]);
+        }
+
+        System.out.println(Arrays.toString(lr_min_height));
+
+        for (int i = 0; i < height.length; i++) {
+            result += lr_min_height[i] - height[i];
+        }
+        return result;
+    }
+}
