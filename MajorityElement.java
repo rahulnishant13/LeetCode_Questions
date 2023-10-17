34bfae75ecfd23b4e19f0b4a67f819b38c0379a0
Rahul Nishant <rahulnishant13@gmail.com>
2023-10-17 14:59:00 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,13 @@
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int result = nums[0];
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if (count ==0) result = nums[i];
+            if(result == nums[i]) count++;
+            else count--;
+        }
+
+        return result;
+    }
+}
