5f98ad546dbe417d0f3be01c2a45c5adfed418e7
Rahul Nishant <rahulnishant13@gmail.com>
2025-02-10 21:23:08 +0530

    Update FindMinimumInRotatedSortedArray.java

--- FindMinimumInRotatedSortedArray.java
+++ FindMinimumInRotatedSortedArray.java
@@ -1,24 +1,27 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/?envType=problem-list-v2&envId=5go9wqm5
+
 package LeetCode_Questions;
 
 public class FindMinimumInRotatedSortedArray {
     public static void main(String[] args) {
         int [] nums = {11,13,15,17};
-        System.out.println(getMin(nums, 0, nums.length-1));
+        System.out.println(findMinNum(nums, 0, nums.length-1));
     }
 
-    private static int getMin(int [] arr, int low, int high){
-        if (low > high)
-            return -1;
-        if(arr[low] <= arr[high])
-            return arr[low];
+    private static int findMinNum(int[] nums, int start, int end){
+        if (start > end) return -1;
+        if(nums[start] <= nums[end]) return nums[start];
+        
+        int mid = (start+end) / 2;
 
-        int mid = (low+high)/2;
+        if(nums[mid] < nums[mid+1] && nums[mid] < nums[mid-1]){
+            return nums[mid];
+        }
 
-        if(mid > 0 && mid < arr.length-1 && arr[mid-1] > arr[mid] && arr[mid] < arr[mid+1])
-            return arr[mid];
-
-        if(arr[mid] > arr[high])
-            return getMin(arr, mid+1, high);
-        return getMin(arr, low, mid);
+        if(nums[mid] > nums[end]){
+            return findMinNum(nums, mid+1, end);
+        } else {
+            return findMinNum(nums, start, mid);
+        }
     }
 }
