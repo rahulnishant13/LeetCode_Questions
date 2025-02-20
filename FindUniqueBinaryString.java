743fb2e3e9290b8ce6db929701907ed4c887ac18
Rahul Nishant <rahulnishant13@gmail.com>
2025-02-20 13:13:56 +0530

    Update Find Unique Binary String

--- FindUniqueBinaryString.java
+++ FindUniqueBinaryString.java
@@ -1,6 +1,35 @@
+// Find Unique Binary String
 // https://leetcode.com/problems/find-unique-binary-string/description/
 
+class FindUniqueBinaryString {
+    public String findDifferentBinaryString(String[] nums) {
+        List<String> result = new ArrayList<>();
+        List<String> numList = Arrays.asList(nums);
 
+        getDifferentBinaryString(result, new StringBuilder(), new String[]{"0", "1"}, numList);
+        return result.get(0);
+    }
+
+    void getDifferentBinaryString(List<String> result, StringBuilder data, String[] nums, List<String> numList){
+        if(data.length() == numList.size()){
+            if(!numList.contains(data.toString())) {
+                result.add(data.toString());
+            }
+            return;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            data.append(nums[i]);
+            getDifferentBinaryString(result, data, nums, numList);
+            data.deleteCharAt(data.length()-1);
+            if(result.size() > 0){
+                return;
+            }
+        }
+    }  
+}
+
+//=======================================================================================
 class FindUniqueBinaryString {
     public String findDifferentBinaryString(String[] nums) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
