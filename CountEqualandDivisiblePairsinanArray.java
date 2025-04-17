abf1ba2bd77b56798e79f6945a76414f455a0362
Rahul Nishant <rahulnishant13@gmail.com>
2025-04-18 00:44:23 +0530

    Create 2176. Count Equal and Divisible Pairs in an Array

---
+++ CountEqualandDivisiblePairsinanArray.java
@@ -0,0 +1,17 @@
+// 2176. Count Equal and Divisible Pairs in an Array
+// https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array
+
+class CountEqualandDivisiblePairsinanArray {
+    public int countPairs(int[] nums, int k) {
+        int result = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i] == nums[j] && i*j % k == 0) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
